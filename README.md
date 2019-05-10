05225c25cbfd80e09737e87172f8e277b9873c2b
Kasım Mert ŞENTÜRK <ksm_mrtsntrk@outlook.com>
2019-05-10 21:24:22 +0300

    mertinkotr

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# kleeja-3.0.2-tr
+
+## Kleeja
+The powerful and easiest way to run File Upload Service on your website.
+Trusted by thousands of webmasters since 2007.
+
+![](https://raw.githubusercontent.com/awssat/kleeja/master/docs/screenshot1.png)
+
+
+## Download (تحميل):
+https://github.com/awssat/kleeja/releases
+
+
+## More Information:
+http://awssat.com/kleeja/
+
+
---
+++ lang/tr/install.php
@@ -0,0 +1,117 @@
+<?php
+//
+// kleeja dil, yönetim
+// Türkçe'ye ÇEVİREN: Kasım Mert ŞENTÜRK --> www.mertsenturk.net <--
+//
+
+
+return array(
+	'DIR'		            => 'ltr',
+	'INST_INSTALL_WIZARD'	=> 'Kleeja installing wizard',
+	'INST_INSTALL_CLEAN_VER'=> 'Yeni Yükleme',
+	'INST_UPDATE_P_VER'		=> 'Güncelleme ',
+	'INST_AGR_LICENSE'		=> 'Şartları ve anlaşmaları kabul ediyorum',
+	'INST_NEXT'		        => 'İleri',
+	'INST_PREVIOUS'	    	=> 'Geri',
+	'INST_SITE_INFO'		=> 'Site Bilgi',
+	'INST_ADMIN_INFO'		=> 'Admin Bilgi',
+	'INST_CHANG_CONFIG'		=> 'Eksik gereksinimler ... config.php dosyasını düzenlediğinizden emin olun.',
+	'INST_CONNCET_ERR'		=> 'Bağlanamıyor ..',
+	'INST_NO_WRTABLE'		=> 'Dizin yazılabilir değil',
+	'INST_GOOD_GO'		=> 'Her şey yolunda gibi görünüyor.... devam',
+	'INST_MSGINS'		=> 'Yükleme hizmetimize hoş geldiniz, burada şartlarımızı ihlal etmediği sürece her şeyi yükleyebilirsiniz.',
+	'INST_CRT_CALL'		=> 'Yorumlar tablosu oluşturuldu.',
+	'INST_CRT_ONL'		=> 'Çevrimiçi kullanıcılar tablosu oluşturuldu.',
+	'INST_CRT_REPRS'	=> 'Raporlar tablosu oluşturuldu.',
+	'INST_CRT_STS'		=> 'İstatistikler tablosu oluşturuldu.',
+	'INST_CRT_USRS'		=> 'Kullanıcılar tablosu oluşturuldu.',
+	'INST_CRT_ADM'		=> 'Yönetici detayları oluşturuldu.',
+	'INST_CRT_FLS'		=> 'Dosya tablosu oluşturuldu.',
+	'INST_CRT_CNF'		=> 'Ayarlar tablosu oluşturuldu.',
+	'INST_CRT_EXT'		=> 'Uzantılar tablosu oluşturuldu.',
+	'INST_CRT_HKS'		=> 'Hacks tablosu oluşturuldu',
+	'INST_CRT_LNG'		=> 'Dil tablosu oluşturuldu',
+
+	'INST_CRT_PLG'		=> 'Eklentiler tablosu oluşturuldu',
+	'INST_SQL_ERR'		=> 'SQL Yürütme Hatası ..',
+	'INST_FINISH_SQL'	=> 'Kleeja başarıyla kuruldu!',
+	'INST_NOTES'		=> 'Yükleme notları ..!',
+	'INST_END'		    => 'Kurulum sihirbazı tamamlandı, Lütfen INSTALL dizinini kaldırın ..!',
+	'INST_NOTE_D'		=> 'Herhangi bir gözlem veya sorun, geliştiriciler kleeja .. ile irtibata geçiniz .. !!',
+	'INST_FINISH_ERRSQL'		=> 'Hata! bir sorun var gibi görünüyor, tekrar deneyin.',
+	'INST_KLEEJADEVELOPERS'		=> 'Kleeja\'yı kullandığınız için teşekkür ederiz ve başarılar dileriz.',
+	'SITENAME'		=> 'Web Site Başlığı',
+	'SITEURL'		=> 'Web Site URL\'si',
+	'SITEMAIL'		=> 'Website Email',
+	'USERNAME'		=> 'Kullanıcı Adı',
+	'PASSWORD'		=> 'Şifre',
+	'PASSWORD2'		=> 'Şifre Tekrar',
+	'EMAIL'		=> 'E-posta',
+	'INDEX'		=> 'Anasayfa',
+	'ADMINCP'		=> 'Kontrol Paneli',
+	'EMPTY_FIELDS'		=> 'Bazı önemli alanlar boş bırakılmıştır!',
+	'WRONG_EMAIL'		=> 'Yanlış e-posta adresi!',
+
+	
+	'DB_INFO_NW'		=> 'Veritabanını doğru bir şekilde girin. Sonra İleri\'ye basın, sihirbaz config.php dosyasını dışa aktaracak ve ana betiği bir dizine koyacaktır!',
+	'DB_INFO'		=> 'Veritabanı bilgilerinizi giriniz ..!',
+	'DB_SERVER'		=> 'Sunucu',
+	'DB_TYPE'		=> 'Veritabanı türü',
+	'DB_TYPE_MYSQL'		=> 'MySQL Standardıd',
+	'DB_TYPE_MYSQLI'		=> 'MySQL Geliştirilmiş',
+	'DB_USER'		=> 'Veritabanı Kullanıcı Adı',
+	'DB_PASSWORD'		=> 'Veritabanı Şifre',
+	'DB_NAME'		=> 'Veritabanı Adı',
+	'DB_PREFIX'		=> 'Tablolar öneki',
+	'VALIDATING_FORM_WRONG'		=> 'Gerekli bir alan boş bırakıldı!',
+	'CONFIG_EXISTS'		=> 'Config.php bulundu, Devam et ...',
+	'INST_SUBMIT_CONFIGOK'		=> 'Dosyayı ana dizine yükleyin',
+	'INST_EXPORT'		=> 'Aktarma dosyası',
+	'INST_OTHER_INFO'		=> 'Diğer Bilgi',
+	'URLS_TYPES'		=> 'Dosya URL\'lerinin stili',
+	'DEFAULT'		    => 'Varsayılan - önerilir',
+	'FILENAME_URL'		=> 'Dosya adı',
+	'DIRECT_URL'		=> 'Doğrudan bağlantılar - istatistik yok',
+	'LIKE_THIS'		    => 'Örnek',
+
+	//
+	'FUNCTIONS_CHECK'		=> 'İşlevler Kontrolü',
+	'RE_CHECK'		=> 'Yeniden Kontrol',
+	'FUNCTION_IS_NOT_EXISTS'		=> '%s işlev devredışı.',
+	'FUNCTION_IS_EXISTS'		=> '%s işlevi etkin.',
+	'FUNCTION_DISC_UNLINK'		=> 'Unlink işlevi, önbellek dosyalarını kaldırmak ve güncellemek için kullanılır.',
+	'FUNCTION_DISC_GD'		=> 'İmagecreatetruecolor işlevi, küçük resimler oluşturmak ve fotoğrafları kontrol etmek için kullanılan GD kitaplığının işlevidir.',
+	'FUNCTION_DISC_FOPEN'		=> 'Fopen işlevi kleeja\'daki stilleri ve dosyaları kontrol etmek için kullanılır.',
+	'FUNCTION_DISC_MUF'		=> 'move_uploaded_file işlevi dosyaları yüklemek için kullanılır ve komut dosyasındaki en önemli işlevdir.',
+	//
+	'ADVICES_CHECK'		=> 'Gelişmiş kontrol (İsteğe bağlı)',
+	'ADVICES_REGISTER_GLOBALS'		=> '<span style="color:red;padding:0 6px;">register_globals fonksiyon etkin ..!</span><br /> devre dışı bırakmanız önerilir.',
+	'ADVICES_MAGIC_QUOTES'		=> '<span style="color:red;padding:0 6px;">magic_quotes fonksiyon etkin ..!</span><br /> devre dışı bırakmanız önerilir.',
+	
+	//UPDATE
+	'INST_CHOOSE_UPDATE_FILE'		=> 'Uygun güncelleme dosyasını seçin',
+	'INST_ERR_NO_SELECTED_UPFILE_GOOD'		=> 'Uygun olmayan güncelleme dosyası veya eksik!',
+	'INST_UPDATE_CUR_VER_IS_UP'		=> 'Mevcut sürümünüz bu güncellemeden daha yeni.',
+	
+	'INST_NOTES_UPDATE'		=> 'Notları Güncelle',
+
+
+	'INST_UPDATE_IS_FINISH'		=> 'Yükleme tamamlandı! Şimdi INSTALL dizinini silebilirsiniz ...',
+
+	'INST_PHP_LESSMIN'		=> 'Kleeja\'yı yüklemek için %1$s veya daha yüksek bir PHP\'ye ihtiyacınız var, mevcut sürümünüz:%2$s',
+	'INST_MYSQL_LESSMIN'		=> 'Kleeja\'yı kurmak için MySQL %1$s ya da daha fazlasına ihtiyacınız var, mevcut sürümünüz:%2$s',
+	'IS_IT_OFFICIAL'		=> 'Kleeja.com adresinden kopyanızı aldınız mı (Kleeja resmi sitesi) ?',
+	'IS_IT_OFFICIAL_DESC'		=> 'Kleeja\'da meydana gelen bazı hataların ve sorunların nedeni hakkında birçok şikayet ve soru alıyoruz ve muhtemelen sorunun ne olduğunu çözemiyoruz. Kontrol ettikten sonra, güvenilmeyen yayıncılardan yayımlanan resmi olmayan bazı kopyaların olduğunu gördük. <span class="sure"> Bu kopyanın Kleeja resmi sitesinden indirildiğinden emin misin?</span>',
+	'INST_WHAT_IS_KLEEJA_T'		=> 'Kleeja Nedir ?',
+	
+	'INST_WHAT_IS_KLEEJA'		=> 'Keeja ücretsiz, zengin özelliklere sahip, dosya ve resim yükleme sistemidir. Kleeja, web yöneticilerinin sitelerinde iyi bir barındırma hizmeti sunmalarına yardımcı olmak için geliştirilmiştir. Kleeja, basit bir kaynak kodu ve güçlü bir Kullanıcı sistemi ile birlikte gelir, ayrıca kolay şablon sistemi sayesinde stillerinizi kolayca özelleştirebilirsiniz ',
+	
+	'INST_SPECIAL_KLEEJA'		=> 'Bazı Kleeja özellikleri .. !',
+	'INST_WHAT_IS_KLEEJA_ONE'		=> 'Kleeja, birçok panoya kolayca entegre edilebilen basit ve güçlü bir kullanıcı sistemine sahiptir. Kleeja, sitenizdeki her şeyi kontrol etmenizi sağlayan basit yönetici kontrol paneli sağlar. Ayrıca Kleeja\'nın stilini kişiselleştirebilir ve birçok eklenti yükleyebilirsiniz .... <a target="_blank" href="http://www.kleeja.com/features/">Kleeja sitesinde daha fazla detay </a>',
+	'YES'		=> 'Evet',
+	'NO'		=> 'Hayır',
+
+	'KLEEJA_TEAM_MSG_NAME'		=> 'Kleeja Geliştirme Ekibi',
+	'KLEEJA_TEAM_MSG_TEXT'		=> "Web sitenizi güçlendirmek için Kleeja’yı seçtiğiniz için teşekkür ederiz.\n Kleeja’nın size sunduğu eşsiz deneyimin tadını çıkarmanızı umuyoruz.\n Gelecekteki güncellemeler için http://kleeja.com adresini ziyaret etmeyi unutmayın.",
+
+);
